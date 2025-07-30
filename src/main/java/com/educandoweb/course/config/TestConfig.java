b3eaa597a810d8e28198275559e1ef461a5568f2
VieiraGuilherme-dev <gv524003@gmail.com>
2025-07-30 16:18:29 -0300

    Payment oneTo-one Association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -29,7 +29,6 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
-
     @Override
     public void run(String... args) throws Exception {
 
@@ -74,7 +73,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-        
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
+
         System.out.println("Dados iniciais para User, Order e Category populados com sucesso!");
     }
 }
